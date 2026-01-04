071809a5a4d306f20cede109d7df54095e8a0856
Shishi <andreiusita6@gmail.com>
2026-01-04 10:52:15 +0800

    Add review section renderer

--- src/home.js
+++ src/home.js
@@ -29,4 +29,40 @@ export class Home extends Restaurant {
 		title.textContent = "Bladie's Pizzeria";
 		titleWrapper.appendChild(title);
 	}
+
+	addReview() {
+		const reviews = [
+			{
+				name: "Yukino",
+				review: "The ramen pizza is good, but the ramen is better, dunno why they call themselves pizzeria, but you get the point!",
+				rating: 10,
+			},
+			{
+				name: "Charlotte",
+				review: "The food is tasty, but it has high concentration of Aetherium according to my tests, if you are not a Luminary, proceed with caution.",
+				rating: 10.
+			}
+		];
+
+		const wrapper = this.wrapper;
+
+		reviews.forEach(index => {
+			const ratingWrapper = document.createElement("div");
+			ratingWrapper.className = "content-wrapper"
+
+			const nameElement = document.createElement("h1");
+			nameElement.textContent = index.name;
+			ratingWrapper.appendChild(nameElement);
+
+			const reviewElement = document.createElement("p");
+			reviewElement.textContent = index.review;
+			ratingWrapper.appendChild(reviewElement);
+
+			const ratingElement = document.createElement("p");
+			ratingElement.textContent = `${index.rating}/10`;
+			ratingWrapper.appendChild(ratingElement);
+
+			wrapper.appendChild(ratingWrapper);
+		})
+	}
 }
